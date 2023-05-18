70abb17b145cae449f40d1327412f6cb81d80308
deankolev6 <114641015+deankolev6@users.noreply.github.com>
2023-05-18 10:15:37 +0300

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[uid] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [user_name] VARCHAR(50) NOT NULL, 
+    [user_age] INT NOT NULL
+)
---
+++ dbo.Table_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[uid] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [user_name] NVARCHAR(50) NOT NULL, 
+    [user_age] INT NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([uid]), 
+    
+    
+)
